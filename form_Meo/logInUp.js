8bdb4858d7c69a706346d618789bc4586c7778ec
Alice M-C <morgane_felloni@hotmail.fr>
2021-04-29 12:25:44 +0200

    :truck: rename js form Meo

--- form_Meo/logInUp.js
+++ form_Meo/logInUp.js
@@ -1,14 +1,3 @@
-
-//* Pour range
-
-function get_tickmarks() {
-    // remplacer .replaceWith avec un équivalent de textContent omg - -"
-    $("#choix").replaceWith("<small id='choix'>&nbsp; => Choix : </small>");
-    let val = document.getElementById("mimique").value;
-    console.log("choix = " + val);
-    $("#choix").append(`${val}`);
-}
-
 //* IDENTIFICATION
 
     //* DECO
