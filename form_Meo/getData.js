f84eb61d9e5a2aaa77edb7946d805d2f8cf0469c
Alice M-C <morgane_felloni@hotmail.fr>
2021-05-04 23:09:01 +0200

    :hammer: affichage data de la DB, réglé

--- form_Meo/getData.js
+++ form_Meo/getData.js
@@ -14,10 +14,10 @@
 
 
 
-// return await fetch(`${thisUrl}/api/form`, {
-// return await fetch('http://localhost:3000/api/question')
 const GetData = async function() {
-        return await fetch('https://aphantasique-form.herokuapp.com/api/question', {
+    // return await fetch('http://localhost:3000/api/question')
+    return await fetch(`${thisUrl}/api/form`, {
+        // return await fetch('https://aphantasique-form.herokuapp.com/api/question', {
         method: "GET", 
         headers: {
           "Content-Type": "application/json", 
@@ -34,105 +34,35 @@ const GetData = async function() {
 }
 GetData();
 
-let promise = fetch(`${thisUrl}/api/form`, {
-    method: "GET", //ou POST, PUT, DELETE, etc.
-    headers: {
-      "Content-Type": "text/plain;charset=UTF-8" //pour un corps de type chaine
-    },
-    body: undefined,
-});
-
+//* Classer par question
 function DisplayQuestion(data) {
+    
+    let dataGraph = [[], [], [], [], [], [], []];
+    
+//* On parcourt l'ensemble des form
+for (let x = 0; x < data.length; x++) {
+    console.log('x', x)
 
-    let question0 = [];
-    let question1 = [];
-    let question2 = [];
-    let question3 = [];
-    let question4 = [];
-    let question5 = [];
-    let question6 = [];
+    //*On trie par numéro de question,
+    //* au cas où les questions n'aient pas été traitées dans l'ordre
+    data[x].Num_Data.sort(function(a, b){
+        a = a[0];
+        b = b[0];
+        console.log(a, b)
+        return a-b
+    });
+    console.log(data[x].Num_Data);
 
-    console.log(data[4])
-    console.log(data[4].Num_Data[0])
-    console.log(data[4].Num_Data[1])
-    console.log(data[4].Num_Data[2])
-    console.log(data[4].Num_Data[3])
-    console.log(data[4].Num_Data[4])
-    console.log(data[4].Num_Data[5])
-    console.log(data[4].Num_Data[6])
-    console.log(data[4].Num_Data[0].length)
-    // console.log(data[4].Num_Data[].length)
-
-
-    for (let i = 0; i < data[4].Num_Data.length; i++) {
-        if (data[4].Num_Data[i].num = 0) {
-            question0.push(data[4].Num_Data[i].data)
-        } else if (data[4].Num_Data[i].num = 1) {
-            question1.push(data[4].Num_Data[i].data)
-        } else if (data[4].Num_Data[i].num = 2) {
-            question2.push(data[4].Num_Data[i].data)
-        } else if (data[4].Num_Data[i].num = 3) {
-            question3.push(data[4].Num_Data[i].data)
-        } else if (data[4].Num_Data[i].num = 4) {
-            question4.push(data[4].Num_Data[i].data)
-        } else if (data[4].Num_Data[i].num = 5) {
-            question5.push(data[4].Num_Data[i].data)
-        } else if (data[4].Num_Data[i].num = 6) {
-            question6.push(data[4].Num_Data[i].data)
+    //* Pour chaque Q on push la data qui correspond
+    for (let i = 0; i < data[x].Num_Data.length; i++) {
+        console.log('i', i)
+        if (data[x].Num_Data[i][0] == i) {
+            console.log('push x i', x, i);
+            dataGraph[i].push(data[x].Num_Data[i][1])
         }
     }
-    console.log(question0, question1, question2, question3, question4, question5, question6);
-    console.log(data[4].Num_Data.num);
-    let datatab = []
-    //* Classer par question
+}
     
-    
-    for (let i = 0; i < data.length; i++) {
-        datatab.push(data[i].Num_Data);
-    }
-    console.log('Données organisées (Num + data): ', datatab);
-    console.log(datatab[0]);
-    console.log(datatab[0][0]);
-    console.log(datatab[0][0][0]);
-    console.log(datatab[0][0][1]);
-    for (let i = 0; i < datatab[0].length; i++) {
-        if (datatab[0][0][i] = 0) {
-            console.log(datatab[0][0][i])
-            question0.push(datatab[0][0][1]);
-        }
-    }
-    console.log(question0);
-    
-    const deleteForm = async function() {
-        
-        fetch(`${thisUrl}/api/form/608ee57114417b3adca695dd`, {
-            method: "DELETE"
-        })
-        .then(response => response.json())
-        .then(json => console.log(json));
-    }
-    // deleteForm();
-    // for (let i = 0; i < datatab.length; i++) { 
+    console.log(dataGraph);
 
-    //     if (datatab[i][0] = 0) {
-    //         question0.push(datatab[i][1]);
-    //     } else if (datatab[i][0] = 1) {
-    //         question0.push(datatab[i][1]);
-    //     } else if (datatab[i][0] = 2) {
-    //         question0.push(datatab[i][1]);
-    //     } else if (datatab[i][0] = 3) {
-    //         question0.push(datatab[i][1]);
-    //     } else if (datatab[i][0] = 4) {
-    //         question0.push(datatab[i][1]);
-    //     } else if (datatab[i][0] = 5) {
-    //         question0.push(datatab[i][1]);
-    //     } else if (datatab[i][0] = 6) {
-    //         question0.push(datatab[i][1]);
-    //     }
-    //     console.log(question0, question1, question2, question3, question4, question5, question6 )
-    // }
-
-    // console.log('Données organisées (Q1) -> section : ', data[0].section,
-    // ', question numéro : ', data[0].num, ', question obligatoire ?', data[0].cat, 
-    // 'datas : ', datatab);
 }
